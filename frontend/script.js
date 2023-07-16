624a1ed1089c1c12185df425e3dd8fd649a8f230
Inknyto <nytosene@gmail.com>
2023-07-16 20:28:18 +0000

    dashboard+table

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,7 @@
 const url = 'http://localhost:3000/electromenager';
 
+
+
 async function getData() {
   try {
     const response = await fetch(url);
@@ -87,28 +89,7 @@ async function getData() {
 }
 
 getData();
- 
 
-
-const electroniqueData = {
-  nom: 'lasttry23test',
-  prix: 'lasttry231123456 FCFA',
-  image: 'lasttry23pdhivhauehrpihahrahiuh'
-};
-
-const electromenagerData = {
-  nom_ap: 'lasttry23test',
-  emp_ap: 'lasttry23Some location',
-  prix_ap: 'lasttry23123456 FCFA',
-  img_ap: 'lasttry23image_url'
-};
-
-const luminaireData = {
-  nom_l: 'lasttry23test',
-  prix_l: 'lasttry23789012 FCFA',
-  description: 'lasttry23Someexcept description',
-  image: 'lasttry23image_url'
-};
 
 function sendPostRequest(endpoint, postData) {
     fetch(`http://localhost:3000/${endpoint}`, {
